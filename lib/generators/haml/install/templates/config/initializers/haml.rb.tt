a2186f21777c1730479c0f19ba7d9e7e72909a2d
Andre Arko <andre@arko.net>
2010-08-15 22:40:59 -0700

    Install generator, ugh, terrible. Initializer begone!

--- lib/generators/haml/install/templates/config/initializers/haml.rb.tt
+++
@@ -1,4 +0,0 @@
-# Load Haml and Sass.
-Haml.init_rails(binding) if defined?(Haml)
-
-Haml::Template.options[:format] = :html5
--- lib/haml-rails.rb
+++ lib/haml-rails.rb
@@ -1,7 +1,14 @@
+require 'haml'
+
 module Haml
   module Rails
     class Railtie < ::Rails::Railtie
       config.generators.template_engine :haml
+
+      config.before_initialize do
+        Haml.init_rails(binding)
+        Haml::Template.options[:format] = :html5
+      end
     end
   end
 end
