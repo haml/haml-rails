f3ec66ff9d9fe285916fd20afd5fca20e3857e23
Andre Arko <andre@arko.net>
2010-10-02 10:12:05 -0700

    Fix Mailer so it can generate views for actions

--- lib/generators/haml/mailer/mailer_generator.rb
+++ lib/generators/haml/mailer/mailer_generator.rb
@@ -3,10 +3,14 @@ require 'generators/haml/controller/controller_generator'
 module Haml
   module Generators
     class MailerGenerator < ControllerGenerator
+      source_root File.expand_path("../templates", __FILE__)
+
     protected
+
       def format
         :text
       end
+
     end
   end
 end
