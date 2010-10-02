8ca63a82d7459f2cdbfcefd6005be7e7b92e0180
Andre Arko <andre@arko.net>
2010-10-02 09:29:20 -0700

    Stop generating ERB templates. They're HAML, dur.

--- lib/generators/haml/scaffold/scaffold_generator.rb
+++ lib/generators/haml/scaffold/scaffold_generator.rb
@@ -1,15 +1,14 @@
-require 'generators/haml'
 require 'rails/generators/erb/scaffold/scaffold_generator'
 
 module Haml
   module Generators
     class ScaffoldGenerator < Erb::Generators::ScaffoldGenerator
-      extend TemplatePath
+      source_root File.expand_path("../templates", __FILE__)
 
       def copy_view_files
         available_views.each do |view|
           filename = filename_with_extensions(view)
-          template template_filename_with_extensions(view), File.join("app/views", controller_file_path, filename)
+          template "#{view}.html.haml", File.join("app/views", controller_file_path, filename)
         end
       end
 
@@ -18,23 +17,20 @@ module Haml
       def copy_form_file
         if options[:form_builder].nil?
           filename = filename_with_extensions("_form")
-          template template_filename_with_extensions("_form"), File.join("app/views", controller_file_path, filename)
+          template "_form.html.haml", File.join("app/views", controller_file_path, filename)
         end
       end
 
-      protected
+    protected
 
-        def available_views
-          %w(index edit show new)
-        end
+      def available_views
+        %w(index edit show new)
+      end
 
-        def handler
-          :haml
-        end
+      def handler
+        :haml
+      end
 
-        def template_filename_with_extensions(name)
-          [name, format, handler, :erb].compact.join(".")
-        end
     end
   end
 end
