ccf2e9d7d58e49a471d2c330149964dc34711b46
Andy Zhang <signupforandy@gmail.com>
2012-04-28 23:07:21 -0700

    Removed '.html' from haml extension in scaffolding to save visual space
    Applied to scaffold_generator and corresponding templates in scaffold directory.

--- lib/generators/haml/scaffold/scaffold_generator.rb
+++ lib/generators/haml/scaffold/scaffold_generator.rb
@@ -8,7 +8,7 @@ module Haml
       def copy_view_files
         available_views.each do |view|
           filename = filename_with_extensions(view)
-          template "#{view}.html.haml", File.join("app/views", controller_file_path, filename)
+          template "#{view}.haml", File.join("app/views", controller_file_path, filename)
         end
       end
 
@@ -17,7 +17,7 @@ module Haml
       def copy_form_file
         if options[:form_builder].nil?
           filename = filename_with_extensions("_form")
-          template "_form.html.haml", File.join("app/views", controller_file_path, filename)
+          template "_form.haml", File.join("app/views", controller_file_path, filename)
         end
       end
 
