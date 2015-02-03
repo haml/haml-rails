8e9147f6ba8dc9592379fc0272876014bebabe89
Ruslan Korolev <rs3@fastmail.com>
2015-02-03 23:06:54 +0300

    refactoring tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,26 +7,24 @@ require 'rails/generators/test_case'
 class TestApp < Rails::Application
   config.root = File.dirname(__FILE__)
 end
-Rails.application = TestApp
 
 module Rails
   def self.root
     @root ||= File.expand_path(File.join(File.dirname(__FILE__), '..', 'tmp', 'rails'))
   end
 end
-Rails.application.config.root = Rails.root
 
 # Call configure to load the settings from
 # Rails.application.config.generators to Rails::Generators
-Rails::Generators.configure! Rails.application.config.generators
+Rails.application.load_generators
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 def copy_routes
-  routes = File.expand_path(File.join(File.dirname(__FILE__), 'fixtures', 'routes.rb'))
+  routes = File.join(File.dirname(__FILE__), 'fixtures', 'routes.rb')
   destination = File.join(Rails.root, "config")
   FileUtils.mkdir_p(destination)
-  FileUtils.cp File.expand_path(routes), destination
+  FileUtils.cp File.expand_path(routes), File.expand_path(destination)
 end
 
 # Asserts the given class exists in the given content. When a block is given,
