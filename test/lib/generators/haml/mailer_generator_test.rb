0f170bcec6d30e3efc350b8142296056b87c5412
Ryan Bigg <me@ryanbigg.com>
2019-01-29 08:59:55 +1100

    Fix warnings with assert_match

--- test/lib/generators/haml/mailer_generator_test.rb
+++ test/lib/generators/haml/mailer_generator_test.rb
@@ -14,53 +14,53 @@ class Haml::Generators::MailerGeneratorTest < Rails::Generators::TestCase
 
     if ::Rails.version.to_s >= '4.2'
       assert_file "app/views/layouts/mailer.text.haml" do |view|
-        assert_match /\= yield/, view
+        assert_match(/\= yield/, view)
       end
 
       assert_file "app/views/layouts/mailer.html.haml" do |view|
-        assert_match /\= yield/, view
+        assert_match(/\= yield/, view)
       end
 
       assert_file "app/views/notifier_mailer/foo.html.haml" do |view|
         assert_match %r(app/views/notifier_mailer/foo\.html\.haml), view
-        assert_match /\= @greeting/, view
+        assert_match(/\= @greeting/, view)
       end
 
       assert_file "app/views/notifier_mailer/bar.html.haml" do |view|
         assert_match %r(app/views/notifier_mailer/bar\.html\.haml), view
-        assert_match /\= @greeting/, view
+        assert_match(/\= @greeting/, view)
       end
 
       assert_file "app/views/notifier_mailer/foo.text.haml" do |view|
         assert_match %r(app/views/notifier_mailer/foo\.text\.haml), view
-        assert_match /\= @greeting/, view
+        assert_match(/\= @greeting/, view)
       end
 
       assert_file "app/views/notifier_mailer/bar.text.haml" do |view|
         assert_match %r(app/views/notifier_mailer/bar\.text\.haml), view
-        assert_match /\= @greeting/, view
+        assert_match(/\= @greeting/, view)
       end
     else
       if ::Rails.version.to_s >= '4.1'
         assert_file "app/views/notifier/foo.html.haml" do |view|
           assert_match %r(app/views/notifier/foo\.html\.haml), view
-          assert_match /\= @greeting/, view
+          assert_match(/\= @greeting/, view)
         end
 
         assert_file "app/views/notifier/bar.html.haml" do |view|
           assert_match %r(app/views/notifier/bar\.html\.haml), view
-          assert_match /\= @greeting/, view
+          assert_match(/\= @greeting/, view)
         end
       end
 
       assert_file "app/views/notifier/foo.text.haml" do |view|
         assert_match %r(app/views/notifier/foo\.text\.haml), view
-        assert_match /\= @greeting/, view
+        assert_match(/\= @greeting/, view)
       end
 
       assert_file "app/views/notifier/bar.text.haml" do |view|
         assert_match %r(app/views/notifier/bar\.text\.haml), view
-        assert_match /\= @greeting/, view
+        assert_match(/\= @greeting/, view)
       end
     end
   end
