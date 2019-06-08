08161e16d652d355c80b877588e26a5a7fa4603c
Akira Matsuda <ronnie@dio.jp>
2019-06-08 17:24:22 +0900

    Avoid polluting toplevel with a test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -20,12 +20,22 @@ Rails.application.load_generators
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
-def copy_routes
-  routes = File.join(File.dirname(__FILE__), 'fixtures', 'routes.rb')
-  destination = File.join(Rails.root, "config")
-  FileUtils.mkdir_p(destination)
-  FileUtils.cp File.expand_path(routes), File.expand_path(destination)
+module Haml
+  module Rails
+    module GeneratorTestHelpers
+      private
+
+      def copy_routes
+        routes = File.join(File.dirname(__FILE__), 'fixtures', 'routes.rb')
+        destination = File.join(::Rails.root, "config")
+        FileUtils.mkdir_p(destination)
+        FileUtils.cp File.expand_path(routes), File.expand_path(destination)
+      end
+    end
+  end
 end
+
+::Rails::Generators::TestCase.include Haml::Rails::GeneratorTestHelpers
 
 # Remove tmp directory when test suite is completed
 MiniTest.after_run do
