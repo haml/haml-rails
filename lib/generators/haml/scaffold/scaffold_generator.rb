6ae2e39661bda3ca47a56aa338e61d480906d37b
Akira Matsuda <ronnie@dio.jp>
2025-09-26 15:00:53 +0900

    Use Rails version specific template template if exists

--- lib/generators/haml/scaffold/scaffold_generator.rb
+++ lib/generators/haml/scaffold/scaffold_generator.rb
@@ -5,19 +5,26 @@ module Haml
     class ScaffoldGenerator < Erb::Generators::ScaffoldGenerator
       source_root File.expand_path("../templates", __FILE__)
 
+      argument :attributes, type: :array, default: [], banner: "field:type field:type"
+
       def copy_view_files
         available_views.each do |view|
-          filename = filename_with_extensions(view)
-          template "#{view}.html.haml", File.join("app/views", controller_file_path, filename)
+          template_filename = find_template_for_rails_version view
+          filename = filename_with_extensions view
+
+          template template_filename, File.join('app/views', controller_file_path, filename)
         end
+
+        template find_template_for_rails_version('partial'), File.join('app/views', controller_file_path, "_#{singular_name}.html.haml")
       end
 
       hook_for :form_builder, :as => :scaffold
 
       def copy_form_file
         if options[:form_builder].nil?
-          filename = filename_with_extensions("_form")
-          template "_form.html.haml", File.join("app/views", controller_file_path, filename)
+          template_filename = find_template_for_rails_version "_form"
+          filename = filename_with_extensions "_form"
+          template template_filename, File.join("app/views", controller_file_path, filename)
         end
       end
 
@@ -29,6 +36,16 @@ module Haml
 
       def handler
         :haml
+      end
+
+      def find_template_for_rails_version(view)
+        find_in_source_paths "#{rails_version}/#{view}.html.haml"
+      rescue Thor::Error
+        "#{view}.html.haml"
+      end
+
+      def rails_version
+        @rails_version ||= "#{::Rails::VERSION::MAJOR}_#{::Rails::VERSION::MINOR}"
       end
     end
   end
